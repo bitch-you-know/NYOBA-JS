fa48aac9463b5662d1144768ad72215493d1e88f
jablay local <jablaylocal@gmail.com>
2024-05-21 08:38:37 +0700

    [note]sebelum mulai

---
+++ trial.js/5typedata.js
@@ -0,0 +1,6 @@
+//type data terbagi menjadi 4
+// 1 string
+//2 number
+//3 boolean
+//4 null
+//5 undfine
---
+++ trial.js/7stringmanipulation.js
@@ -0,0 +1,21 @@
+// 1 yang pertama adalah menambahkan
+// value + value
+
+// 2 trik membuat spasi pada value  dengan membuat spasi di dalam string akhir 
+//   atau memasukannya di console.log 
+
+// 3 bisa memanipulasi kalimat yang ada di dalam value tanpa merubah langsung dari isi 
+//   value tersebut 
+
+//   contoh untuk membuat awalan value gunakan cari aja di string manipulation  ada art nya gitu
+
+
+//   untuk memotong atau mengambil hanya sebagaian dari sttring ktia bisa menggunakan
+//   .slice
+
+//   untuk membuat paragraf depannya huruf besar kita dapat menggunakan ofupercaps
+let enigma ="Enigma Bootcamp Keren"
+let kata   =" Mantap !"
+let stringMethode ="Awesome is " + enigma.slice(0,14)+kata
+
+console.log (stringMethode)
