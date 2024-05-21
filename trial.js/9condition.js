6b4c6e8a0295d1eb08fbda53790d57df5cdf0115
abi fathan yusuf <abiefathan333@gmail.com>
2024-05-21 17:20:53 +0700

    [NOTED]gua baru beres chapter 8

---
+++ trial.js/9condition.js
@@ -0,0 +1,36 @@
+// adalah suatu keadaan di mana akan terjadi nya eksekusi langsung aja kita check it dot
+
+// const price = 10_000
+// const payment = 3_000
+
+// if (payment===price){
+//     console.log("terimakasih sudah membayar")
+// }
+// else if (payment < price){
+//     console.log(price - payment)
+// }
+
+// else if (payment-price){
+//     console.log(payment-price) }
+// 
+
+
+const myAnswer = "a"
+
+switch(myAnswer){
+
+    case "a":
+    console.log("jawaban salah")
+    break
+
+    case "b":
+        console.log("jawaban benar")
+        break
+    case "c":
+       console.log("jawaban salah")
+        break
+
+     case "a":
+         console.log("jawaban salah")
+         break
+}
---
+++ trial.js/percobaan.js
@@ -0,0 +1,12 @@
+// let w = 3.60
+// let x = "12"
+// let m =81
+// let z = 4.58
+
+
+// // let result = Math.floor(w) + Math.floor(z)+m+ Number(x)
+
+// console.log(Math.floor(w)+Math.floor(z)+String(m)+x)
+let babi =9
+
+console.log (babi)
