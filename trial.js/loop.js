d2fdd3eb00c8789e60f339df1f4967b1591eb848
abi fathan yusuf <abiefathan333@gmail.com>
2024-05-23 00:56:08 +0700

    [note]malam jam 1

---
+++ trial.js/loop.js
@@ -0,0 +1,8 @@
+//loop -> adalah condition yang berulang selama condition masih true
+//segmen 1 adalah di eksekusi sebelum loop jallan
+//segmen 2 adalah condition  (true|false
+//segmen 3 adalah di exsekusi di akhir sebuah loop
+
+for (let i = 0; i <5 ; i ++){
+    console.log("masuk loop")
+}
